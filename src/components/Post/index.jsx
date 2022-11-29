4311b8fe1bfcb6c80a03fb2d618ad20e1b3cee1f
Carlos Lopes <dev.carloslopes@gmail.com>
2022-11-29 00:29:43 -0300

    Adiciona o conteúdo e a estilização do componente Post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -5,16 +5,29 @@ import styles from './post.module.css';
 export function Post() {
   return (
     <article className={styles.post}>
-      <header>
+      <header className={styles['post-header']}>
         <div className={styles.author}>
-          <img src="https://github.com/devcarlosl.png" />
-          <div>
+          <img className={styles['author-avatar']} src="https://github.com/devcarlosl.png" />
+          <div className={styles['author-info']}>
             <strong>Carlos lopes</strong>
             <span>Desenvolvedor Web</span>
           </div>
         </div>
-        <span>Publicado há 1h</span>
+        <time title='28 de Novembro às 23:41h' dateTime='2022-11-28 23:41:00' className={styles['post-published-ago']}>Publicado há 1h</time>
       </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉<a href='#'>{' '}jane.design/doctorcare</a></p>
+
+        <p>
+          <a href='#'>#novoprojeto</a>{' '}
+          <a href='#'>#nlw</a>{' '}
+          <a href='#'>#rocketseat</a>
+        </p>
+      </div>
     </article>
   );
 }
